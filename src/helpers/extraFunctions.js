dd5cc77729dfa0700173052ed41b5f9234802a37
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-05-02 21:47:40 +0530

    local date conversion function added

--- src/helpers/extraFunctions.js
+++ src/helpers/extraFunctions.js
@@ -6,4 +6,17 @@ export const myToast = (toast, title, status, description) => toast({
     status,
     duration: 5000,
     isClosable: true,
-})
+});
+
+export const dateFormat = (dt) => {
+
+    const milliseconds = dt * 1000;
+
+    let myDate = new Date(milliseconds);
+
+    let date = myDate.toLocaleString().split(",")[0];
+
+    let day = myDate.toLocaleString("en-US", { weekday: "long" });
+
+    return { date, day };
+}
